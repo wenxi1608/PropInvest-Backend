571824b8070925eaa0eddf9a0ea9fb8353d7f791
wenxi1608 <wenxihe1@gmail.com>
2022-10-31 02:42:00 +0800

    fix: updated rental routes by year

--- controllers/property_controller.js
+++ controllers/property_controller.js
@@ -2,8 +2,7 @@ const axios = require("axios");
 const cors = require("cors");
 
 module.exports = {
-  // To amend the refPeriod
-  getRentalData: async (req, res) => {
+  getRentalData2022: async (req, res) => {
     try {
       const tokenResponse = await axios.get(
         `https://www.ura.gov.sg/uraDataService/insertNewToken.action`,
@@ -16,7 +15,7 @@ module.exports = {
       const tokenData = await tokenResponse.data;
       const token = tokenData.Result;
 
-      const rentalResponse = await axios.get(
+      const firstQtr2022 = await axios.get(
         `https://www.ura.gov.sg/uraDataService/invokeUraDS?service=PMI_Resi_Rental&refPeriod=22q1`,
         {
           headers: {
@@ -25,10 +24,154 @@ module.exports = {
           },
         }
       );
-      const rentalData = await rentalResponse.data;
-      return res.json(rentalData);
+      const secondQtr2022 = await axios.get(
+        `https://www.ura.gov.sg/uraDataService/invokeUraDS?service=PMI_Resi_Rental&refPeriod=22q2`,
+        {
+          headers: {
+            AccessKey: `${process.env.ACCESS_KEY}`,
+            Token: token,
+          },
+        }
+      );
+      const thirdQtr2022 = await axios.get(
+        `https://www.ura.gov.sg/uraDataService/invokeUraDS?service=PMI_Resi_Rental&refPeriod=22q3`,
+        {
+          headers: {
+            AccessKey: `${process.env.ACCESS_KEY}`,
+            Token: token,
+          },
+        }
+      );
+      const allQtrData = firstQtr2022.data.Result.concat(
+        secondQtr2022.data.Result,
+        thirdQtr2022.data.Result
+      );
+      const rentalData2022 = await allQtrData;
+      return res.json(rentalData2022);
     } catch (err) {
-      console.log("Unable to get rental transaction data");
+      console.log("Unable to get rental transaction data", err);
+    }
+  },
+
+  getRentalData2021: async (req, res) => {
+    try {
+      const tokenResponse = await axios.get(
+        `https://www.ura.gov.sg/uraDataService/insertNewToken.action`,
+        {
+          headers: {
+            AccessKey: `${process.env.ACCESS_KEY}`,
+          },
+        }
+      );
+      const tokenData = await tokenResponse.data;
+      const token = tokenData.Result;
+
+      const firstQtr2021 = await axios.get(
+        `https://www.ura.gov.sg/uraDataService/invokeUraDS?service=PMI_Resi_Rental&refPeriod=21q1`,
+        {
+          headers: {
+            AccessKey: `${process.env.ACCESS_KEY}`,
+            Token: token,
+          },
+        }
+      );
+      const secondQtr2021 = await axios.get(
+        `https://www.ura.gov.sg/uraDataService/invokeUraDS?service=PMI_Resi_Rental&refPeriod=21q2`,
+        {
+          headers: {
+            AccessKey: `${process.env.ACCESS_KEY}`,
+            Token: token,
+          },
+        }
+      );
+      const thirdQtr2021 = await axios.get(
+        `https://www.ura.gov.sg/uraDataService/invokeUraDS?service=PMI_Resi_Rental&refPeriod=21q3`,
+        {
+          headers: {
+            AccessKey: `${process.env.ACCESS_KEY}`,
+            Token: token,
+          },
+        }
+      );
+      const fourthQtr2021 = await axios.get(
+        `https://www.ura.gov.sg/uraDataService/invokeUraDS?service=PMI_Resi_Rental&refPeriod=21q4`,
+        {
+          headers: {
+            AccessKey: `${process.env.ACCESS_KEY}`,
+            Token: token,
+          },
+        }
+      );
+      const allQtrData = firstQtr2021.data.Result.concat(
+        secondQtr2021.data.Result,
+        thirdQtr2021.data.Result,
+        fourthQtr2021.data.Result
+      );
+      const rentalData2021 = await allQtrData;
+      return res.json(rentalData2021);
+    } catch (err) {
+      console.log("Unable to get rental transaction data", err);
+    }
+  },
+
+  getRentalData2020: async (req, res) => {
+    try {
+      const tokenResponse = await axios.get(
+        `https://www.ura.gov.sg/uraDataService/insertNewToken.action`,
+        {
+          headers: {
+            AccessKey: `${process.env.ACCESS_KEY}`,
+          },
+        }
+      );
+      const tokenData = await tokenResponse.data;
+      const token = tokenData.Result;
+
+      const firstQtr2020 = await axios.get(
+        `https://www.ura.gov.sg/uraDataService/invokeUraDS?service=PMI_Resi_Rental&refPeriod=20q1`,
+        {
+          headers: {
+            AccessKey: `${process.env.ACCESS_KEY}`,
+            Token: token,
+          },
+        }
+      );
+      const secondQtr2020 = await axios.get(
+        `https://www.ura.gov.sg/uraDataService/invokeUraDS?service=PMI_Resi_Rental&refPeriod=20q2`,
+        {
+          headers: {
+            AccessKey: `${process.env.ACCESS_KEY}`,
+            Token: token,
+          },
+        }
+      );
+      const thirdQtr2020 = await axios.get(
+        `https://www.ura.gov.sg/uraDataService/invokeUraDS?service=PMI_Resi_Rental&refPeriod=20q3`,
+        {
+          headers: {
+            AccessKey: `${process.env.ACCESS_KEY}`,
+            Token: token,
+          },
+        }
+      );
+      const fourthQtr2020 = await axios.get(
+        `https://www.ura.gov.sg/uraDataService/invokeUraDS?service=PMI_Resi_Rental&refPeriod=20q4`,
+        {
+          headers: {
+            AccessKey: `${process.env.ACCESS_KEY}`,
+            Token: token,
+          },
+        }
+      );
+      const allQtrData = firstQtr2020.data.Result.concat(
+        secondQtr2020.data.Result,
+        thirdQtr2020.data.Result,
+        fourthQtr2020.data.Result
+      );
+      const rentalData2020 = await allQtrData;
+      return res.json(rentalData2020);
+    } catch (err) {
+      console.log("Unable to get rental transaction data", err);
     }
   },
 
--- routers/property_routes.js
+++ routers/property_routes.js
@@ -3,7 +3,9 @@ const propertyController = require("../controllers/property_controller");
 const router = express.Router();
 
 // Rental
-router.get("/rental-transactions", propertyController.getRentalData);
+router.get("/rental-transactions-2022", propertyController.getRentalData2022);
+router.get("/rental-transactions-2021", propertyController.getRentalData2021);
+router.get("/rental-transactions-2020", propertyController.getRentalData2020);
 // Median Rental PSF
 router.get("/median-rental-psf", propertyController.getMedianRentalPsfData);
 // Sale
